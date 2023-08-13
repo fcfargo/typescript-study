7ae7b5700d739c6aa22d8a0a088459b4df49c53c
fcfargo <fcfargo@gmail.com>
2023-08-13 20:52:12 +0900

    refactor: destructure read() method of CsvFileReader class(to work with any type of CSV file)

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -3,6 +3,8 @@ import { CsvFileReader } from "./CsvFileReader";
 const reader = new CsvFileReader("football.csv");
 reader.read();
 
+console.log(reader.data);
+
 // let manUnitedWins = 0;
 
 // for (let match of reader.data) {
