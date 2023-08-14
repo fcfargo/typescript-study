c0df8eb9b0babb03cc80ed215e378088d02d5ac2
fcfargo <fcfargo@gmail.com>
2023-08-14 20:59:00 +0900

    refactor: transform CsvFileReader into abstract class by using generics

--- stats/src/CsvFileReader.ts
+++ stats/src/CsvFileReader.ts
@@ -1,12 +1,12 @@
 import fs from "fs";
-import { dateStringToDate } from "./Utils";
 import { MatchResult } from "./MatchResult";
 
-type MatchData = [Date, string, string, number, number, MatchResult, string];
+export abstract class CsvFileReader<T> {
+  data: T[] = [];
 
-export class CsvFileReader {
-  data: MatchData[] = [];
   constructor(public filename: string) {}
+
+  abstract mapRow(row: string[]): T;
 
   read(): void {
     this.data = fs
@@ -16,17 +16,5 @@ export class CsvFileReader {
         return row.split(",");
       })
       .map(this.mapRow);
-  }
-
-  mapRow(row: string[]): MatchData {
-    return [
-      dateStringToDate(row[0]),
-      row[1],
-      row[2],
-      parseInt(row[3]),
-      parseInt(row[4]),
-      row[5] as MatchResult,
-      row[6],
-    ];
   }
 }
