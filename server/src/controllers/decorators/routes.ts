825d2fb3b21eeb42290c1c6df0986fe80af8ea23
fcfargo <fcfargo90@gmail.com>
2024-05-09 11:48:54 +0900

    feat: add routeBinder

--- server/src/controllers/decorators/routes.ts
+++ server/src/controllers/decorators/routes.ts
@@ -1,7 +1,16 @@
 import 'reflect-metadata';
 
-export function Get(path: string) {
-  return function (target: any, key: string, desc: PropertyDescriptor): void {
-    Reflect.defineMetadata('path', path, target, key);
+function routeBinder(method: string) {
+  return function (path: string) {
+    return function (target: any, key: string, desc: PropertyDescriptor): void {
+      Reflect.defineMetadata('method', method, target, key);
+      Reflect.defineMetadata('path', path, target, key);
+    };
   };
 }
+
+export const Get = routeBinder('get');
+export const Post = routeBinder('post');
+export const Put = routeBinder('put');
+export const Delete = routeBinder('delete');
+export const Patch = routeBinder('patch');
