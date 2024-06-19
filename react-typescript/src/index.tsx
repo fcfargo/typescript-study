9af75d96098ef64cc7282b232d26186b2e228128
fcfargo <fcfargo90@gmail.com>
2024-06-19 21:08:56 +0900

    feat: add Component method and initialize state variable

--- react-typescript/src/index.tsx
+++ react-typescript/src/index.tsx
@@ -9,10 +9,35 @@ interface AppProps {
   color?: string;
 }
 
-class App extends Component<AppProps> {
+interface AppState {
+  counter: number;
+}
+
+class App extends Component<AppProps, AppState> {
+  constructor(props: AppProps) {
+    super(props);
+    this.state = { counter: 0 };
+    this.onIncrement = this.onIncrement.bind(this);
+    this.onDecrement = this.onDecrement.bind(this);
+  }
+
+  onIncrement(): void {
+    this.setState({ counter: this.state.counter + 1 });
+  }
+
+  onDecrement(): void {
+    this.setState({ counter: this.state.counter - 1 });
+  }
+
   render() {
-    return <div>{this.props.color}</div>;
+    return (
+      <div>
+        <button onClick={this.onIncrement}>Increment</button>
+        <button onClick={this.onDecrement}>Decrement</button>
+        {this.state.counter}
+      </div>
+    );
   }
 }
 
-root.render(<App />);
+root.render(<App color={'color'} />);
