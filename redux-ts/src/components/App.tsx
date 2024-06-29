0593b591285c18c6bf68b888ae260af336180189
fcfargo <fcfargo90@gmail.com>
2024-06-29 11:43:20 +0900

    feat: add RepositoriesList component

--- redux-ts/src/components/App.tsx
+++ redux-ts/src/components/App.tsx
@@ -1,13 +1,17 @@
 import { Provider } from 'react-redux';
 import reducers from '../state/reducers';
 import { configureStore } from '@reduxjs/toolkit';
+import RepositoriesList from './RepositoriesList';
 
 const store = configureStore({ reducer: reducers });
 
 const App = () => {
   return (
     <div>
-      <Provider store={store}>Hello there!</Provider>
+      <Provider store={store}>
+        <h1>Search For a Package</h1>
+        <RepositoriesList />
+      </Provider>
     </div>
   );
 };
